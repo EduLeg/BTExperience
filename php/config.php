68538720edf28982fe4f5498fc0e3f8a867c34e9
Angel009 <angelus007.ans@gmail.com>
2021-01-26 17:45:45 -0600

    implementación de claves de acceso a la BD

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@ define("KEY","ETDMBTE");
 define("COD","AES-128-ECB");
 
 define("SERVIDOR","localhost");
-define("USUARIO","root");
-define("PASSWORD","");
-define("BD","Registro");
+define("USUARIO","u400794863_admin");
+define("PASSWORD","w>M6|QRrb!3*");
+define("BD","u400794863_BTE");
 ?>
